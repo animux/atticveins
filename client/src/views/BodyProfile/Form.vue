64fe6dad3d3d9cb7d7bda839ad8c7d3b7167240b
animuxx <djami1562@gmail.com>
2021-02-11 13:49:14 +0600

    Body Profile Form Interface changed

--- client/src/views/BodyProfile/Form.vue
+++ client/src/views/BodyProfile/Form.vue
@@ -2,9 +2,22 @@
     <div id="form">
         <b-container>
             <b-form @submit="onSubmit" validated="true">
-                <b-form-group id="name" label="Name" label-for="name">
-                    <b-form-input id="name=input" type="text" placeholder="Enter Name" required></b-form-input>
-                </b-form-group>
+                
+                <section v-if="steps == 1">
+                    <b-form-group id="name" label="Name" label-for="name">
+                        <b-form-input id="name=input" type="text" placeholder="Enter Name" required></b-form-input>
+                    </b-form-group>
+                    <b-form-group id="name" label="Name" label-for="name">
+                        <b-form-input id="name=input" type="text" placeholder="Enter Name" required></b-form-input>
+                    </b-form-group>
+                    <b-form-group id="name" label="Name" label-for="name">
+                        <b-form-input id="name=input" type="text" placeholder="Enter Name" required></b-form-input>
+                    </b-form-group>
+                </section>
+
+                <section v-if="steps == 2">
+                    
+                </section>
             </b-form>
         </b-container>
     </div>
@@ -12,10 +25,30 @@
 
 <script>
 export default {
-    name: 'Form'
+    name: 'Form',
+    data() {
+        return {
+            totalsteps: 3,
+            steps: 1,
+            form: {
+
+            }
+        }
+    }
 }
 </script>
 
 <style lang="scss" scoped>
-
+    #form {
+        position: relative;
+        width: 50%;
+        margin: 0 auto;
+        color: white;
+        form {
+            background-color: #1A1A1A;
+            border-radius: 5px;
+            box-shadow: 0 8px 4em -6px #090909;
+            padding: 40px 40px;
+        }
+    }
 </style>
