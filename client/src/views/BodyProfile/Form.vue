f848bbf04e4b103aa3115b8fe8530d19cfb95ffd
animux <djami1562@gmail.com>
2021-02-18 16:01:52 +0600

    BodyProfile Form layout

--- client/src/views/BodyProfile/Form.vue
+++ client/src/views/BodyProfile/Form.vue
@@ -5,40 +5,62 @@
                 <h3 v-bind:class="{ active: this.steps == 1 }">About your Body</h3>
                 <h3 v-bind:class="{ active: this.steps == 2 }">About yourself</h3>
             </div>
-            <b-form validated="true">
+            <b-form>
                 <section v-if="steps == 1">
+                    <b-row>
+                        <b-col lg="6">
+                            <b-form-group id="current-weight" label="Current Weight" label-for="weight">
+                                <b-form-input id="current-weight-input" type="text" placeholder="Enter your current weight" required></b-form-input>
+                            </b-form-group>
+                        </b-col>
+                        <b-col lg="6">
+                            <b-form-group id="desired-weight" label="Desired Weight" label-for="desired-weight">
+                                <b-form-input id="desired-weight-input" type="text" placeholder="Enter your desired weight" required></b-form-input>
+                            </b-form-group>
+                        </b-col>
+                    </b-row>
                     <b-form-group id="height" label="Height" label-for="height">
                         <b-form-input id="height-input" type="text" placeholder="Enter your height" required></b-form-input>
                     </b-form-group>
-                    <b-form-group id="current-weight" label="Current Weight" label-for="weight">
-                        <b-form-input id="current-weight-input" type="text" placeholder="Enter your current weight" required></b-form-input>
-                    </b-form-group>
-                    <b-form-group id="desired-weight" label="Desired Weight" label-for="desired-weight">
-                        <b-form-input id="desired-weight-input" type="text" placeholder="Enter your desired weight" required></b-form-input>
-                    </b-form-group>
                     <b-form-group id="name" label="Activity Level" label-for="name">
-                        <b-form-input id="name=input" type="text" placeholder="Enter Name" required></b-form-input>
+                        <b-form-select :options="form.activityOption"></b-form-select>
                     </b-form-group>
-                    <b-form-group id="name" label="Name" label-for="name">
-                        <b-form-input id="name=input" type="text" placeholder="Enter Name" required></b-form-input>
-                    </b-form-group>
-                    <b-form-group id="name" label="Name" label-for="name">
-                        <b-form-input id="name=input" type="text" placeholder="Enter Name" required></b-form-input>
-                    </b-form-group>
-                    <b-form-group id="name" label="Name" label-for="name">
-                        <b-form-input id="name=input" type="text" placeholder="Enter Name" required></b-form-input>
+                    <b-row>
+                        <b-col lg="6">
+                            <b-form-group id="name" label="Neck Size" label-for="neck-size">
+                                <b-form-input id="neck-size-input" type="text" placeholder="Enter your neck size" required></b-form-input>
+                            </b-form-group>
+                        </b-col>
+                        <b-col lg="6">
+                            <b-form-group id="name" label="Waist Size" label-for="waist-size">
+                                <b-form-input id="waist-size-input" type="text" placeholder="Enter your weist size" required></b-form-input>
+                            </b-form-group>
+                        </b-col>
+                    </b-row>
+
+
+                    <b-form-group id="name" label="Medical Issues (if any)" label-for="medical-issue">
+                        <b-form-textarea
+                            id="textarea"
+                            placeholder="Enter your medical issue (if any)"
+                            rows="3"
+                            max-rows="6"
+                        ></b-form-textarea>
                     </b-form-group>
                 </section>
 
                 <section v-if="steps == 2">
-                    <b-form-group id="name" label="Age" label-for="name">
-                        <b-form-input id="name=input" type="text" placeholder="Enter DOB" required></b-form-input>
-                    </b-form-group>
                     <b-form-group id="name" label="Name" label-for="name">
-                        <b-form-input id="name=input" type="text" placeholder="Enter Name" required></b-form-input>
+                        <b-form-input id="name-input" type="text" placeholder="Enter your name" required></b-form-input>
                     </b-form-group>
-                    <b-form-group id="name" label="Name" label-for="name">
-                        <b-form-input id="name=input" type="text" placeholder="Enter Name" required></b-form-input>
+                    <b-form-group id="phone-number" label="Phone Number" label-for="phone-number">
+                        <b-form-input id="phone-number-input" type="text" placeholder="Enter your phone number" required></b-form-input>
+                    </b-form-group>
+                    <b-form-group id="email" label="Email" label-for="email">
+                        <b-form-input id="email-input" type="text" placeholder="Enter your email" required></b-form-input>
+                    </b-form-group>
+                    <b-form-group id="age" label="Age" label-for="age">
+                        <b-form-input id="age-input" type="text" placeholder="Enter your age" required></b-form-input>
                     </b-form-group>
                 </section>
 
@@ -60,7 +82,16 @@ export default {
         return {
             totalsteps: 2,
             steps: 1,
-            form: {}
+            form: {     
+                activityOption: [
+                    { value: null, text: 'Please select an option' },
+                    { value: '1', text: 'Sedentary' },
+                    { value: '2', text: 'Workout 1 or 2 days a week' },
+                    { value: '3', text: 'Active workout 4 to 5  days a week' },
+                    { value: '4', text: 'Intense workout every week' },
+                ],
+                data: {}
+            }
         }
     },
     methods: {
@@ -82,6 +113,7 @@ export default {
         width: 50%;
         margin: 0 auto;
         color: white;
+        text-align: center;
         .title {
             color: gray;
             text-align: center;
