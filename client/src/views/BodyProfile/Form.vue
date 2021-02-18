8fb62c6604ac374c3925af44b140498021783e21
animuxx <djami1562@gmail.com>
2021-02-18 15:09:53 +0600

    BodyProfile Form Step 1

--- client/src/views/BodyProfile/Form.vue
+++ client/src/views/BodyProfile/Form.vue
@@ -1,10 +1,24 @@
 <template>
     <div id="form">
         <b-container>
+            <div class="title">
+                <h3 v-bind:class="{ active: this.steps == 1 }">About your Body</h3>
+                <h3 v-bind:class="{ active: this.steps == 2 }">About yourself</h3>
+            </div>
             <b-form validated="true">
-                
                 <section v-if="steps == 1">
-                    <h3>Step 1</h3>
+                    <b-form-group id="height" label="Height" label-for="height">
+                        <b-form-input id="height-input" type="text" placeholder="Enter your height" required></b-form-input>
+                    </b-form-group>
+                    <b-form-group id="current-weight" label="Current Weight" label-for="weight">
+                        <b-form-input id="current-weight-input" type="text" placeholder="Enter your current weight" required></b-form-input>
+                    </b-form-group>
+                    <b-form-group id="desired-weight" label="Desired Weight" label-for="desired-weight">
+                        <b-form-input id="desired-weight-input" type="text" placeholder="Enter your desired weight" required></b-form-input>
+                    </b-form-group>
+                    <b-form-group id="name" label="Activity Level" label-for="name">
+                        <b-form-input id="name=input" type="text" placeholder="Enter Name" required></b-form-input>
+                    </b-form-group>
                     <b-form-group id="name" label="Name" label-for="name">
                         <b-form-input id="name=input" type="text" placeholder="Enter Name" required></b-form-input>
                     </b-form-group>
@@ -17,22 +31,8 @@
                 </section>
 
                 <section v-if="steps == 2">
-                    <h3>Step 2</h3>
                     <b-form-group id="name" label="Age" label-for="name">
                         <b-form-input id="name=input" type="text" placeholder="Enter DOB" required></b-form-input>
-                    </b-form-group>
-                    <b-form-group id="name" label="Name" label-for="name">
-                        <b-form-input id="name=input" type="text" placeholder="Enter Name" required></b-form-input>
-                    </b-form-group>
-                    <b-form-group id="name" label="Name" label-for="name">
-                        <b-form-input id="name=input" type="text" placeholder="Enter Name" required></b-form-input>
-                    </b-form-group>
-                </section>
-
-                <section v-if="steps == 3">
-                    <h3>Step 3</h3>
-                    <b-form-group id="name" label="DOB" label-for="name">
-                        <b-form-input id="name=input" type="text" placeholder="Enter Age" required></b-form-input>
                     </b-form-group>
                     <b-form-group id="name" label="Name" label-for="name">
                         <b-form-input id="name=input" type="text" placeholder="Enter Name" required></b-form-input>
@@ -58,7 +58,7 @@ export default {
     name: 'Form',
     data() {
         return {
-            totalsteps: 3,
+            totalsteps: 2,
             steps: 1,
             form: {}
         }
@@ -82,6 +82,24 @@ export default {
         width: 50%;
         margin: 0 auto;
         color: white;
+        .title {
+            color: gray;
+            text-align: center;
+            display: grid;
+            grid-template-columns: repeat(2, 1fr);
+            h3 {
+                border-bottom: 1px solid gray;
+                padding: 10px 0;
+                transition: ease-in 0.3s;
+                margin-bottom: 20px;
+                text-transform: uppercase;
+            }
+            .active {
+                color: white;
+                transition: ease-out 0.3s;
+                border-bottom: 1px solid white;
+            }
+        }
         form {
             background-color: #1A1A1A;
             border-radius: 5px;
